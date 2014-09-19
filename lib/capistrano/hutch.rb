98cc37db23feecc6d1b05e7b5b1c24c33e5a0534
Rustam Sharshenov <rustam@sharshenov.com>
2014-09-19 14:02:47 +0600

    Initial commit

---
+++ lib/capistrano/hutch.rb
@@ -0,0 +1,7 @@
+require "capistrano/hutch/version"
+
+module Capistrano
+  module Hutch
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/hutch/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Hutch
+    VERSION = "0.0.1"
+  end
+end
